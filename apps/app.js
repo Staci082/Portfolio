6dc028037dd0d33b2bc4bc65d1bfd8a4ffd9992f
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-04-26 11:39:32 +0200

    hamburger backgorund color fixed

--- apps/app.js
+++ apps/app.js
@@ -2,15 +2,20 @@
 // hamburger menu
 
 const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".navbar");
 const navMenu = document.querySelector(".navbar-menu");
+
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    nav.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.
   addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    nav.classList.remove("active");
   }))
