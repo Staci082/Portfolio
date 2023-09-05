af56430f71736e1d56e3fd9bb5aed562b320f271
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-09-05 10:38:18 +0200

    test package fix

--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -7,50 +7,30 @@ function Projects() {
             title: "Fiesta Flavors",
             image: "/images/music-app.png",
             desc: "Discover, save and organize your beloved recipes using an online platform. ",
-            stack: [
-                "sass",
-                "react.js",
-                "node.js",
-                "express.js",
-                "mongoDB",
-                "cheerio",
-            ],
-            website: "",
+            stack: ["sass", "react.js", "node.js", "express.js", "mongoDB", "cheerio"],
+            website: ""
         },
         {
             id: "card2",
             title: "Epic Games clone",
             image: "",
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus.",
-            stack: [
-                "tailwind",
-                "react.js",
-                "node.js",
-                "express.js",
-                "fetch api",
-            ],
-            website: "",
+            stack: ["tailwind", "react.js", "node.js", "express.js", "fetch api"],
+            website: ""
         },
         {
             id: "card3",
             title: "Coming soon",
             image: "",
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dignissimos, minus.",
-            stack: [
-                "sass",
-                "ejs",
-                "node.js",
-                "express.js",
-                "mongoDB",
-                "something.js",
-            ],
-            website: "",
-        },
+            stack: ["sass", "ejs", "node.js", "express.js", "mongoDB", "something.js"],
+            website: ""
+        }
     ];
 
     return (
         <section id="projects-container">
-            <h2>projects</h2>
+            <h3>projects</h3>
 
             <div className="projects-wrapper">
                 {projectList.map((item) => (
@@ -58,36 +38,29 @@ function Projects() {
                         <div className="card-img"></div>
                         <div className="details">
                             <div className="title-container">
-                                <h3 className="h3">{item.title}</h3>
+                            <h4 className="h4">{item.title}</h4>
+                            
+                            <a href="#" className="link-button"><FaArrowUpRightFromSquare/></a>
 
-                                <a href="#" className="link-button">
-                                    <FaArrowUpRightFromSquare />
-                                </a>
+
                             </div>
 
                             <p>{item.desc}</p>
 
-                            <h3 className="h3">Stack:</h3>
+                            <h4 className="h4">Stack:</h4>
                             <ul>
                                 {item.stack.map((stack, index) => {
-                                    return (
-                                        <li className="li" key={index}>
-                                            {stack}
-                                        </li>
-                                    );
+                                    return <li className="li" key={index}>{stack}</li>
                                 })}
                             </ul>
+                            
+                            
                         </div>
                     </div>
                 ))}
             </div>
 
-            <a
-                className="github"
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/Staci082"
-            >
+            <a className="github" target="_blank" rel="noreferrer" href="https://github.com/Staci082">
                 <FaGithub />
             </a>
         </section>
