38042ba05e55b0ab3cc4d5dbe86822e475502d5d
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-09-05 11:27:24 +0200

    Update About.jsx

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return (
         <section id="home-container">
             <div id="title">
-                <h1>staci <br/> johnson<hr/><h6 className="under-title">Aspiring Web Developer</h6></h1>
+                <h1>staci <br/> johnson<hr/><h4 className="under-title">Aspiring Web Developer</h4></h1>
                 
             </div>
         </section>
