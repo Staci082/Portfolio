72317f719fd691208de49f7b693e344687f6f24b
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-09-07 13:22:27 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Projects from "./Components/projects/Projects";
 
 function App() {
 
-    const sitekey = import.meta.env.VITE_SITE_KEY || process.env.SITEKEY
+    const sitekey = import.meta.env.VITE_SITE_KEY || process.env.VITE_SITEKEY
     
     const handleVerify = (token) => {
         console.log('ReCaptcha token:', token)
