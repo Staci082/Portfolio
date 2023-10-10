83a8a77732e8b1d44f55d04aef230c68281cc7c5
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-10 09:28:22 +0200

    undertitle margin fixed

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return (
         <section id="home-container">
             <div id="title">
-                <h1>staci <br/> johnson<hr/></h1><h4 className="under-title">Aspiring Web Developer</h4>
+                <h1>staci <br/> johnson<hr/></h1><b className="under-title">Aspiring Web Developer</b>
                 
             </div>
         </section>
